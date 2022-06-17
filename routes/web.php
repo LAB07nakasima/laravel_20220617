75e643c64e1255d1a45e80e6e5fbfec66abf43f6
LAB07nakasima <rumor0301@icloud.com>
2022-06-17 07:17:35 +0000

    laravel laratter

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TweetController;
+use App\Http\Controllers\FavoriteController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// ここでログインしていないユーザーはアクセスできないようにする
+Route::group(['middleware' => 'auth'], function () {
+  // favoriteとunfavoriteのurlを設定
+  Route::post('tweet/{tweet}/favorites', [FavoriteController::class, 'store'])->name('favorites');
+  Route::post('tweet/{tweet}/unfavorites', [FavoriteController::class, 'destroy'])->name('unfavorites');
+  // 🔽 追加
+  Route::get('/tweet/mypage', [TweetController::class, 'mydata'])->name('tweet.mypage');
+  Route::resource('tweet', TweetController::class);
+});
+
+Route::get('/', function () {
+  return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+  return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
